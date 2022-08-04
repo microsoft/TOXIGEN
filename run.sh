5e47dc7f9c3987028ce5085409c85cbbf9716cf0
Thomas Hartvigsen <twhartvigsen@wpi.edu>
2022-08-04 07:57:37 -0400

    switched default run to GPT3

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-#python generate.py --input_prompt_file ./prompts/hate_black_1k.txt --language_model GPT2 --output_file test_file.txt --num_generations_per_prompt 10
-python generate.py --input_prompt_file ./prompts/hate_black_1k.txt --language_model GPT2 --classifier RoBERTa --ALICE True --output_file test_file.txt --num_generations_per_prompt 10
+python generate.py --input_prompt_file ./prompts/hate_black_1k.txt --language_model GPT3 --classifier RoBERTa --ALICE True --output_file test_file.txt --num_generations_per_prompt 10 --generation_mode hate --endpoint_url <URL_HERE> --api_key <API_KEY>
